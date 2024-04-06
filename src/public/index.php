cf18efdb9a84835fdb94a3b31c9b5e25b46b9f4b
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-04-06 15:45:33 +0900

    fix: for文のデバッグのコードをクラス化

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,50 @@
+<!-- 実践課題1(for文のデバッグ)のコードをクラス化しましょう。 -->
+<?php 
+
+class TotalCalculator {
+  private $total;
+
+  public function __construct() {
+    $this->total = 0;
+  }
+  
+  public function calculateTotal() {
+    for ($i = 0; $i < 5; $i++) {
+      $this->total += $i;
+      var_dump($this->total);
+    }
+  }
+}
+
+// インスタンス化
+$calculator = new TotalCalculator();
+
+// メソッドを呼び出して処理を実行
+$calculator->calculateTotal();
+?>
+
+
+<!-- 実践課題2(for文のデバッグ)のコードをクラス化しましょう。 -->
 <?php
-echo 'Welcome TECH QUEST!';
+
+class TotalCalculator2 {
+  private $total;
+
+  public function __construct() {
+    $this->total = 0;
+  }
+
+  public function calculateTotal() {
+    for ($i = 0; $i < 15; $i++) {
+      $this->total += $i;
+      var_dump($this->total);
+    }
+  }
+}
+
+// インスタンス化
+$calculator = new TotalCalculator2();
+
+// メソッドを呼び出して処理を実行
+$calculator->calculateTotal();
+?>
